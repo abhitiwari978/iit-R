da29704fa2f24c159294f17b0a783ef087a51858
abhitiwari978 <gopaltiwarigopal786@gmail.com>
2024-08-22 22:58:01 +0530

    Updating for headway management

--- check.py
+++ check.py
@@ -1,11 +1,18 @@
 import pandas as pd
-schedule=pd.read_excel("328-H New_ Summary_Schedule_3min_Headway 40 buses 20240815.xlsx",sheet_name="Schedule")
-schedule
-limit=15
-limit = pd.to_timedelta(f'00:{limit}:00')  
-hour=7
-schedule['start_time'] = pd.to_datetime(schedule['start_time'], format='%H:%M:%S')
-schedule['headway'] = pd.to_timedelta(schedule['headway'])
-exceeded_df = schedule[(schedule['start_time'].dt.hour == hour) & 
-                       (schedule['headway'] > limit)]
-print(exceeded_df)
+
+# Load the Excel file
+schedule = pd.read_excel("328-H New_ Summary_Schedule_3min_Headway 40 buses 202408155.xlsx", sheet_name="Schedule")
+
+# Define the headway limit
+limit =15  # 15 minutes limit
+hour = 7
+
+
+# Filter rows where headway exceeds the limit and 'start_time' hour is 7
+exceeded_df = schedule[
+    (pd.to_datetime(schedule['headway']).dt.minute > limit) &
+    (pd.to_datetime(schedule['start_time']).apply(lambda x: x.hour) == hour)
+]
+output_file_path = 'check1.csv'
+schedule_df = pd.DataFrame(exceeded_df)
+schedule_df.to_csv(output_file_path, index=False)
